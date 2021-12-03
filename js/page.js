3b17435703f0e0633c569b5c2d9eefa31a791a07
SofieMangino <manginsm@clarkson.edu>
2021-12-03 13:56:19 -0500

    Trying to make a modal

--- js/page.js
+++ js/page.js
@@ -25,3 +25,14 @@ function showSlides(n) { // creates the showSlides function using n as the param
   slides[slideIndex-1].style.display = "block"; // display slides
   indicator[slideIndex-1].className += " active"; // make indicator active
 }
+
+$(".button").on("click", function() { // when item with class of button is clicked, fire function
+  var modal = $(this).data("modal"); // sets modal var equal to data attribute
+  $(modal).show(); // opens up modal (much code hidden here by jQuery)
+});
+$(".modal").on("click", function(e) { // sets up click function
+  var className = e.target.className; // var className set to event target
+  if(className === "modal" || className === "close"){ // if conditions are met...
+    $(this).closest(".modal").hide(); // then hide the modal
+  } // closes if-statement
+}); // closes function
